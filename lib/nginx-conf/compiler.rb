351e8c27e616c28a64edb3c7e4ed149653c1b4a4
Yota Toyama <raviqqe@gmail.com>
2016-10-23 13:12:08 +0900

    Better error message

--- lib/nginx-conf/compiler.rb
+++ lib/nginx-conf/compiler.rb
@@ -19,7 +19,7 @@ class Compiler
     when -> (o) { o.respond_to? :to_s }
       object.to_s
     else
-      exit "You sucks!"
+      exit "The object doesn't respond to to_s method.: #{o}"
     end
   end
 
