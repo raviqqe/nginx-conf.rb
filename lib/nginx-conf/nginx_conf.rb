cd854b6f19f0edc3eb38b330d0334e7392d35817
Yota Toyama <raviqqe@gmail.com>
2016-10-26 02:05:24 +0900

    Replace if_ with if

--- lib/nginx-conf/nginx_conf.rb
+++ lib/nginx-conf/nginx_conf.rb
@@ -6,5 +6,42 @@ require_relative 'compiler'
 
 def nginx_conf &block
   repeats = %i(server load_module fastcgi_param set if_)
-  Compiler.new(repeats).compile block_is_hash(repeats, &block)
+
+  hash = block_is_hash repeats, &block
+  replace_if = lambda { |o| replace_elem :if_, :if, o }
+  replace_if.call hash
+  replace_if.call repeats
+  Compiler.new(repeats).compile hash
+end
+
+
+def replace_elem origin, subst, object
+  replace = lambda { |o| replace_elem origin, subst, o }
+
+  case object
+  when Hash
+    new_pairs = {}
+
+    object.each do |key, value|
+      case origin
+      when key
+        object.delete key
+        new_pairs[subst] = value
+      when value
+        object[key] = subst
+      else
+        replace.call value
+      end
+    end
+
+    object.merge! new_pairs
+  when Array
+    object.each_with_index do |value, key|
+      if value == origin
+        object[key] = subst
+      else
+        replace.call value
+      end
+    end
+  end
 end
