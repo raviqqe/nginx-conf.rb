7a88a2808d021542153b8ba6898ae33daec3a86a
Yota Toyama <raviqqe@gmail.com>
2016-10-25 12:58:41 +0900

    Add fastcgi_param to repeats

--- lib/nginx-conf/nginx_conf.rb
+++ lib/nginx-conf/nginx_conf.rb
@@ -5,6 +5,6 @@ require_relative 'compiler'
 
 
 def nginx_conf &block
-  repeats = %i(server load_module)
+  repeats = %i(server load_module fastcgi_param)
   Compiler.new(repeats).compile block_is_hash(repeats, &block)
 end
