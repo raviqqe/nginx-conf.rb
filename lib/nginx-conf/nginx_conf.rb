c21b2be26fd2549f8a7dc725be3975621b513519
Yota Toyama <raviqqe@gmail.com>
2016-10-29 13:06:30 +0900

    Refactor nginx_conf function

--- lib/nginx-conf/nginx_conf.rb
+++ lib/nginx-conf/nginx_conf.rb
@@ -7,10 +7,8 @@ require_relative 'compiler'
 
 def nginx_conf &block
   repeats = %i(server load_module fastcgi_param set if_)
-
-  hash = block_is_hash repeats, &block
-
-  Compiler.new(replace_elems repeats).compile replace_elems(hash)
+  Compiler.new(replace_elems repeats)
+      .compile replace_elems(block_is_hash(repeats, &block))
 end
 
 
